9117bc45b493fbb32131dcd2f2df4f455764e1ac
ALTeriorMotive <lightning.aaron1221@gmail.com>
2025-06-20 10:23:55 -0500

    VS html assignment

---
+++ WebAppTutorial/WebAppTutorial/obj/Release/net8.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function newColor() {
+    document.getElementById('hello_world').style.color = 'blue';
+}
+
+function oldColor() {
+    document.getElementById('hello_world').style.color = 'darkolivegreen';
+}
+
+function videoEnlarge() {
+    document.getElementById('video').style.width = '60%';
+    document.getElementById('video').style.margin = '0 20%';
+}
+
+function videoShrink() {
+    document.getElementById('video').style.width = '30%';
+    document.getElementById('video').style.margin = '0 35%';
+}
